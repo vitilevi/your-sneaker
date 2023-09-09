b39aa3ba66453de592517b421491cd12f2d52d9b
Victor Faria <vitilevi@hotmail.com>
2023-09-09 01:29:23 -0300

    Add phosphor to dependencies, and update public html

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import NikeLogo from './nike-logo.png';
+import IconProfile from './icon-profile.png';
+import IconSearch from './icon-search.png';
+import IconShop from './icon-shop.png';
+import Sneaker0 from './sneaker0.png';
+import Sneaker1 from './sneaker1.png';
+import Sneaker2 from './sneaker2.png';
+import Sneaker3 from './sneaker3.png';
+
+export default {
+  NikeLogo,
+  IconProfile,
+  IconSearch,
+  IconShop,
+  Sneaker0,
+  Sneaker1,
+  Sneaker2,
+  Sneaker3,
+}
